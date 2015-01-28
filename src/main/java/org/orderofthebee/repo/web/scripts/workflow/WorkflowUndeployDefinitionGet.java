a2af9bf92754e07a8869709cf80e84182efedb8b
Martin Cosgrave <martin@ciderpunk.com>
2015-01-28 12:20:56 +0100

    remove incorrect alfresco attribution

--- src/main/java/org/orderofthebee/repo/web/scripts/workflow/WorkflowUndeployDefinitionGet.java
+++ src/main/java/org/orderofthebee/repo/web/scripts/workflow/WorkflowUndeployDefinitionGet.java
@@ -1,21 +1,3 @@
-/*
- * Copyright (C) 2005-2011 Alfresco Software Limited.
- *
- * This file is part of Alfresco
- *
- * Alfresco is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Alfresco is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
- */
 package org.orderofthebee.repo.web.scripts.workflow;
 
 import java.util.HashMap;
@@ -31,13 +13,7 @@ import org.springframework.extensions.webscripts.Status;
 import org.springframework.extensions.webscripts.WebScriptException;
 import org.springframework.extensions.webscripts.WebScriptRequest;
 
-/**
- * Webscript impelementation to return the latest version of a workflow
- * definition.
- * 
- * @author Frederik Heremans
- * @since 3.4.e
- */
+
 public class WorkflowUndeployDefinitionGet extends AbstractWorkflowWebscript {
 	private static final String PARAM_WORKFLOW_DEFINITION_ID = "workflowDefinitionId";
 
