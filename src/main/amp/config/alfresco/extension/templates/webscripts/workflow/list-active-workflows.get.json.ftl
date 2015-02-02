cfa6642913f7131b3938ca7c55bd4726f688a7e2
Martin Cosgrave <martin@ciderpunk.com>
2015-02-02 20:03:09 +0100

    various tests

--- src/main/amp/config/alfresco/extension/templates/webscripts/workflow/list-active-workflows.get.json.ftl
+++ src/main/amp/config/alfresco/extension/templates/webscripts/workflow/list-active-workflows.get.json.ftl
@@ -1,19 +1,21 @@
-[
-<#if workflows?has_content>
-<#list workflows as w>
-{
-	"id": "${w.id}",		
-	"active": "${w.active}",		
-	"context": "${w.context}",		
-	"definition": "${w.definition}",		
-	"description": "${w.description}",		
-	"dueDate": "${w.dueDate}",		
-	"endDate": "${w.endDate}",		
-	"initiator": "${w.initiator}",		
-	"priority": "${w.priority}",		
-	"startDate": "${w.startDate}",		
-	"workflowPackage": "${w.workflowPackage}",		
-}<#if (w_index+1) < workflows?size>,</#if>
-</#list>
-</#if>
-]
+{	
+	"data": [
+	<#if workflows?has_content>
+	<#list workflows as w>
+		{
+			"id": "${w.id}",		
+			"active": "${w.active}",		
+			"context": "${w.context}",		
+			"definition": "${w.definition}",		
+			"description": "${w.description}",		
+			"dueDate": "${w.dueDate}",		
+			"endDate": "${w.endDate}",		
+			"initiator": "${w.initiator}",		
+			"priority": "${w.priority}",		
+			"startDate": "${w.startDate}",		
+			"workflowPackage": "${w.workflowPackage}",		
+		}<#if (w_index+1) < workflows?size>,</#if>
+	</#list>
+	</#if>
+	]
+}
--- src/main/java/org/orderofthebee/repo/web/scripts/workflow/WorkflowStartPost.java
+++ src/main/java/org/orderofthebee/repo/web/scripts/workflow/WorkflowStartPost.java
@@ -8,6 +8,7 @@ import org.alfresco.repo.web.scripts.workflow.AbstractWorkflowWebscript;
 import org.alfresco.repo.web.scripts.workflow.WorkflowModelBuilder;
 import org.alfresco.service.cmr.workflow.WorkflowPath;
 import org.alfresco.service.namespace.QName;
+import org.apache.log4j.Logger;
 import org.springframework.extensions.webscripts.Cache;
 import org.springframework.extensions.webscripts.Status;
 import org.springframework.extensions.webscripts.WebScriptRequest;
@@ -15,23 +16,34 @@ import org.springframework.extensions.webscripts.WebScriptRequest;
 public class WorkflowStartPost extends AbstractWorkflowWebscript {
 	private static final String PARAM_WORKFLOW_DEFINITION_ID = "workflowDefinitionId";
 
+	private Logger log = Logger.getLogger(WorkflowStartPost.class);
+	
 	@Override
 	protected Map<String, Object> buildModel(WorkflowModelBuilder modelBuilder,
 			WebScriptRequest req, Status status, Cache cache) {
 
 		// copy some stuff from alfresco WorkflowUndeployDefinitionGet
 
+		log.info("Starting buildmodel");
+		
 		Map<String, String> params = req.getServiceMatch().getTemplateVars();
 
+		for(String key: params.keySet()){
+			log.info("key=" + key + ", val=" + params.get(key));
+		}
+		
 		// Get the definition id from the params
 		String workflowDefinitionId = params.get(PARAM_WORKFLOW_DEFINITION_ID);
 
+		log.info("workflow definition id = " + workflowDefinitionId);
 		
 		Map<QName, Serializable> wparams = new HashMap<QName, Serializable>();
 		
 		// TODO no idea what to pass for wparams
 		
 		WorkflowPath wf = workflowService.startWorkflow(workflowDefinitionId, wparams);
+		
+		log.info("workflow id" + wf.getId());
 		
 		Map<String, Object> model = new HashMap<String, Object>();
 		
